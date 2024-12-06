a774f75dc68526eff775b5b0bb32a1eb401a4e64
ray <enciso.leo@gmail.com>
2024-12-06 15:26:35 -0800

    maquetacion al 30%

--- tiendapp/views.py
+++ tiendapp/views.py
@@ -12,3 +12,14 @@ def v_cart(request):
         "items": [None, None, None, None]
     }
     return  render(request, "tiendapp/cart.html", context)
+
+def v_product_detail(request):
+    context = {}
+    return render(request, 
+                  "tiendapp/product_detail.html",
+                  context)
+    # Completar 
+    # crear un diccionario contexto vacio:  context = {}
+    # El return render , enlazando el html,  
+    # tiendap/product_detail.html
+    # Adicionar a la funcion render, contexto
